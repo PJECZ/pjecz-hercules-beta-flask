f57ea5251f1698958d8d7aa8a9b53cbae1ba4b36
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2026-09-14 16:34:12 -0600

    Modelos DGT

---
+++ pjecz_hercules_beta_flask/blueprints/dgt_rutas/models.py
@@ -0,0 +1,40 @@
+"""
+DGT Rutas modelos
+"""
+
+import uuid
+
+from sqlalchemy import ForeignKey, String
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from pjecz_hercules_beta_flask.config.extensions import database
+from pjecz_hercules_beta_flask.lib.universal_mixin import UniversalMixin
+
+
+class DgtRuta(database.Model, UniversalMixin):
+    """DgtRuta"""
+
+    # Nombre de la tabla
+    __tablename__ = "dgt_rutas"
+
+    # Clave primaria
+    id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+
+    # Claves foráneas
+    dgt_deposito_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("dgt_depositos.id"))
+    dgt_deposito: Mapped["DgtDepositos"] = relationship(back_populates="dgt_rutas")
+    dgt_tipo_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("dgt_tipos.id"))
+    dgt_tipo: Mapped["DgtTipo"] = relationship(back_populates="dgt_rutas")
+
+    # Columnas
+    clave: Mapped[str] = mapped_column(String(16), unique=True)
+    directorio: Mapped[str] = mapped_column(String(512))
+
+    # Hijos
+    dgt_digitalizaciones: Mapped[list["DgtDigitalizacion"]] = relationship(back_populates="dgt_ruta")
+    dgt_entregas: Mapped[list["DgtEntrega"]] = relationship(back_populates="dgt_ruta")
+
+    def __repr__(self):
+        """Representación"""
+        return f"<DgtRuta {self.id}>"
---
+++ pjecz_hercules_beta_flask/blueprints/dgt_tipos/models.py
@@ -0,0 +1,33 @@
+"""
+DGT Tipos modelos
+"""
+
+import uuid
+
+from sqlalchemy import String
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from pjecz_hercules_beta_flask.config.extensions import database
+from pjecz_hercules_beta_flask.lib.universal_mixin import UniversalMixin
+
+
+class DgtTipo(database.Model, UniversalMixin):
+    """DgtTipo"""
+
+    # Nombre de la tabla
+    __tablename__ = "dgt_tipos"
+
+    # Clave primaria
+    id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+
+    # Columnas
+    clave: Mapped[str] = mapped_column(String(16), unique=True)
+    descripcion: Mapped[str] = mapped_column(String(256))
+
+    # Hijos
+    dgt_rutas: Mapped[list["DgtRuta"]] = relationship(back_populates="dgt_tipo")
+
+    def __repr__(self):
+        """Representación"""
+        return f"<DgtTipo {self.id}>"
